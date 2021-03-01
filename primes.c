e385a567d7894be1d10d8235a1d6bb2a4451affd
shadov <kuznja16@gmail.com>
2021-03-01 18:11:45 +0100

    Zmenil jsem soubor

---
+++ primes.c
@@ -0,0 +1,21 @@
+// Solution for IJC-DU1, task a), 28.2.2021
+// File:        bitset.h
+// Author:      Jakub Kuzník, FIT
+// School acco. xkuzni04
+// Compiled:    gcc 9.9.3.0
+
+#include <stdio.h>
+#include "primes.h"
+
+
+
+int main()
+{
+    my_hello();
+    bit_hello();
+    return 0; 
+}
+
+/*
+fprintf(stderr, "Time=%.3g\n", (double)(clock()-start)/CLOCKS_PER_SEC);
+*/     
